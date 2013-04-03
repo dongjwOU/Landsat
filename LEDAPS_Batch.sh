0fb677cc81c86fd35c323d7da91d17056ee3f5bc
johnpconnors <johnpconnors@gmail.com>
2013-04-02 23:44:32 -0300

    Update LEDAPS_Batch.sh

--- LEDAPS_Batch.sh
+++ LEDAPS_Batch.sh
@@ -4,21 +4,29 @@
 #
 
 # unzips downloaded tar files
-for i in *.tar.gz;
-	echo $i
-	do tar -xvzf  $i;
-done;
-
-# executes LEDAPS processes
-for j in *_MTL.txt;
-	do do_LEDAPS.csh $j
-done;
-
-# Extract Bands to TIFF
-for k in lndsr*hdf; 
-	do gdal_translate HDF4_EOS:EOS_GRID:\"$k\":Grid:band3 $i\b3.tif; done
-	do gdal_translate HDF4_EOS:EOS_GRID:\"$k\":Grid:band4 $i\b4.tif; done
-done;
+for i in *.tar.gz; do
+    name=${i//".tar.gz"}
+    echo $name
+    #mkdir working/$name                                                        
+    cd working/$name
+    cat ../../topscript > runjob
+    echo " tar -xvzf ../../$i" >> runjob                                        
+    echo " lndpm *_MTL.txt" >> runjob                                          
+    echo " lndcal lndcal*txt" >> runjob                                        
+    echo " lndcsm lndcsm*txt" >> runjob                                        
+    echo " lndsr lndsr*txt" >> runjob
+    echo " rm *$name*tif" >> runjob
+    # Extract Bands to TIFF                                                     
+    k=lndsr.$name.hdf
+    echo " gdal_translate -co TFW=yes HDF4_EOS:EOS_GRID:\"$k\":Grid:band3 $name_b3.ti\
+f" >> runjob
+    echo " gdal_translate -co TFW=yes HDF4_EOS:EOS_GRID:\"$k\":Grid:band4 $name_b4.ti\
+f" >> runjob
+    echo " ./grass_cluster.sh" >> runjob
+    echo " ./NDVI_calc.sh" >> runjob
+    qsub runjob
+    cd ../..
+done
 
 #Calculate NDVI
 
